988d0a8d98f013138b6a6fad5a37161bb4e759ce
keegan <ksmith3@iastate.edu>
2024-11-04 13:00:47 -0600

    1-4

--- Hello.js
+++ Hello.js
@@ -1,1 +1,50 @@
-console.log("Hello");
+// https://www.learn-js.org/
+
+// 1 basic output
+console.log("Hello!");
+console.log("Goodbye");
+
+// 2 variables
+// note: number type can be integer or float
+
+var myNum = 3; // number type
+var myNum2 = 3.2
+var myString = "WOW"; // string type
+var boolean = true; // boolean type
+
+var myArray = []; // array type
+var myObj = {}; // "object" type
+
+// const can also be used
+const myNum3 = 4;
+
+// 3 Arrays
+// note: in JS, also functions as a List, Stack, and Queue
+// note: Arrays in JS are sparse
+
+var myArray2 = [3, 2, 1];
+// or
+//var MyArray3 = new Array(1,2,3);
+
+// sparsity example
+myArray[3] = 10;
+
+// can store muiltiple objects of different types
+var MyArray4 = [5, "Monday", {}];
+
+// 4 Arrays II
+
+// Arrays have stack capabilities
+var MyStack = [];
+MyStack.push(1);
+MyStack.push(2);
+MyStack.push(3);
+console.log(MyStack);
+
+MyStack.pop();
+console.log(MyStack);
+
+// Arrays have Queue capabilities
+
+
+
